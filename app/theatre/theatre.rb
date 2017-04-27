5188a4d5baa67e8a584dfde828e99c7d7584e58e
gitpetr <pkhilenko@yandex.ru>
2017-04-27 06:52:59 +0600

    delete initialize @cash

--- app/theatre/theatre.rb
+++ app/theatre/theatre.rb
@@ -21,12 +21,7 @@ class Theatre < MovieCollection
   }
 
   include CashBox::MovieCash
-  attr_accessor :cash
-  def initialize filmfile
-    super 
-    @cash = 0
-  end
-
+ 
   def show(hour)
     raise "В указанное время сеансов нет \n" unless (MORNING.first..EVENING.last).cover?(hour)
     period = TIMES.detect{ |p, hours| hours.cover?(hour) }[0]  
